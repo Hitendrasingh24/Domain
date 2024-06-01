2766a77d7398c7d776e54b42a434ba769fa10fac
Hitendrasingh24 <hitendrasingh511@gmail.com>
2024-06-01 16:10:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const https = require('https');
 
 const apiKey = 'at_LDdS5hSmKoNUVG5j6whjj85yolJ6e';
-const domainName = 'google.com';
+const domainsToCheck = [
+  'google.com',
+  'axsolab.com',
+  'example3sdasd.com',
+  'exampleasdas4.io',
+  'examplesdasd5.xyz',
+  'exampleasdasd6.co'
+]; // Replace with your desired domains
 
+const availableDomains = [];
+
+
+function Availability(domainName) {
 const options = {
   hostname: 'www.whoisxmlapi.com',
   port: 443,
@@ -14,23 +25,18 @@ const req = https.request(options, (res) => {
 const numbers = new Array();
   res.on('data', (d) => {
     const ar = String(d);
-    // process.stdout.write(ar);
     numbers.push(ar.length)
   });
 
   res.on('end', () => {
-    if (numbers.length> 10){
-        console.log("OPPS! domain is taken")
-    }
-    else{
-        console.log("domain is available")
+    if (numbers.length< 10){
+      console.log(domainName)
     }
   });
 });
-
 req.on('error', (error) => {
   console.error(error);
 });
-
 req.end();
-
+}
+domainsToCheck.forEach(Availability);
